6b2ac9711f17e41cc40cbdd0d0c129b936dda62b
Freny11 <freny127@gmail.com>
2022-09-04 21:55:57 +0530

    Update provider.tf

--- Challenge#1/Terraform GKE/provider.tf
+++ Challenge#1/Terraform GKE/provider.tf
@@ -27,7 +27,5 @@ provider "helm" {
     host  = module.gke.gke_cluster_endpoint
     token = data.google_client_config.default.access_token
 
-    cluster_ca_certificate = base64decode(module.gke.gke_cluster_ca_certificate)
-
   }
 }
